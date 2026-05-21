b35491c9dc25207aa1f71caba5081b9f5371b423
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-05-21 19:07:17 +0800

    Navbar active links

--- src/features/auth/components/AuthLinks.tsx
+++ src/features/auth/components/AuthLinks.tsx
@@ -1,21 +1,36 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import SignOutLink from "@/features/auth/components/SignOutLink";
 
 import { AUTH_LINKS } from "@/features/auth/utils/constants";
+import { cn } from "@/lib/utils";
 
 export default function AuthLinks() {
+  const pathname = usePathname();
+
   return (
     <>
-      {AUTH_LINKS.map((authLink) => (
-        <Link
-          key={authLink.href}
-          href={authLink.href}
-          className="text-sm font-medium hover:underline underline-offset-4"
-        >
-          {authLink.label}
-        </Link>
-      ))}
+      {AUTH_LINKS.map((authLink) => {
+        const isActive = authLink.href === pathname;
+
+        return (
+          <Link
+            key={authLink.href}
+            href={authLink.href}
+            className={cn(
+              "text-sm font-medium hover:underline underline-offset-4",
+              {
+                underline: isActive,
+              },
+            )}
+          >
+            {authLink.label}
+          </Link>
+        );
+      })}
 
       <SignOutLink />
     </>
