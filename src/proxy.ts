0e8fb7fd8fc332853ae9b96110b3da5a7ff8f89c
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-05-21 18:57:44 +0800

    Proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,1 +1,8 @@
-export { auth as proxy } from "@/auth";
+import { auth } from "@/auth";
+
+export const proxy = auth((req) => {
+  if (!req.auth && req.nextUrl.pathname.startsWith("/dashboard")) {
+    const newUrl = new URL("/sign-in", req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
