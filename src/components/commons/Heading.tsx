b22327ab792ac89eaccadf88436be5faffd07a2b
Mario Ferdinand Corpus <mcorpus.dev@gmail.com>
2026-05-27 07:53:05 +0800

    Update heading

--- src/components/commons/Heading.tsx
+++ src/components/commons/Heading.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 export default function Heading({ title, description }: Props) {
   return (
-    <div className="space-y-2">
+    <div>
       <h3 className="text-lg font-bold tracking-tight">{title}</h3>
 
       <p className="text-muted-foreground">{description}</p>
